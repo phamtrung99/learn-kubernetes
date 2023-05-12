f44763271bf8af409f259572535ec3485ba707d8
Trung Pham <trung.pham@executionlab.asia>
2023-05-12 12:01:31 +0700

    update

--- resources_delete.sh
+++ resources_delete.sh
@@ -6,4 +6,4 @@ kubectl delete secret web-demo-secret -n space2
 kubectl delete deployment mysql-deployment -n space2
 kubectl delete persistentvolumeclaim mysql-pv-claim -n space2
 kubectl delete persistentvolume mysql-pv-volume
-
+kubectl delete namespace space2
